c3b120253a623c631370754a5a97b5b368969f2f
Sárdi András <sardi.andras@gmail.com>
2021-04-13 16:11:50 +0200

    Create Folyoviz.java

---
+++ Folyoviz.java
@@ -0,0 +1,10 @@
+public abstract class Folyoviz extends Viz {
+    protected int folyasiSebesseg;
+
+    public Folyoviz(boolean tiszta, int meret, int folyasiSebesseg){
+        super(tiszta,meret);
+        this.folyasiSebesseg = folyasiSebesseg;
+    }
+
+    public int sodrasiErosseg() {return meret * folyasiSebesseg;}
+}
